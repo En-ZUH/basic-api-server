4a391f56eb02332ff849c707b2ef8285f26737d4
En-ZUH <enas_z_bataineh108@yahoo.com>
2021-07-10 18:06:38 +0300

    done

--- src/models/food.js
+++ src/models/food.js
@@ -1,37 +1,39 @@
 'use strict';
-const uuid = require('uuid').v4;
+const uuid = require('uuid').v4; //to generate a random ID
 
 class Food {
-    constructor() {
-        this.db = [];
-    }
+	constructor() {
+		this.db = [];
+	}
 
-    creat(object) {
-        const food = { id: uuid(), data: object };
-        this.db.push(food);
-        return food;
+	creat(object) {
+		const record = { id: uuid(), record: object };
+		this.db.push(record);
+		return record;
 
-    }
-    get(id) {
-        if (id)
-            return this.db.find(kind => kind.id === id);
-        return this.db;
-    }
+	}
+	get(id) {
+		if (id) {
+			return this.db.find(record => record.id === id);
+		}
+		else return this.db;
 
-    update(id, food_object) {
-        let k;
-        this.db.forEach(kind => {
-            if (kind.id === id) {
-                kind.data = food_object;
-                p = kind;
-            }
-        });
-        return k;
-    }
+	}
 
-    delete(id) {
-        this.db = this.db.filter(kind => kind.id !== id);
-        return this.db;
-    }
+	update(id, food_object) {
+
+		for (let i = 0; i < this.db.length; i++) {
+			let k = this.db[i];
+			if (k.id === id) {
+				k.record = food_object;
+				return k;
+			}
+		}
+	}
+
+	delete(id) {
+		this.db = this.db.filter(record => record.id !== id);
+		return this.db;
+	}
 }
 module.exports = Food;
